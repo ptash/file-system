5bf52458a685664405697925ad7115a908ae9cd1
Tatiana Poray <tanya@interprogma.ru>
2016-10-09 02:01:41 +0300

    TMKGROUP-529 filesystem unit test

---
+++ tests/FileSystemTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Testing FileSystem.
+ *
+ * @package Cognitive\FileSystem\Tests
+ */
+
+namespace Cognitive\FileSystem\Tests;
+
+use Cognitive\FileSystem\FileSystem;
+
+/**
+ * Class FileSystemTest
+ */
+class FileSystemTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Testing emptyDirectory.
+     *
+     * @return void
+     */
+    public function testEmptyDirectory()
+    {
+        $fileSystem = new FileSystem();
+        $dirName = \sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'fileSystemTmpDir';
+        $fileName = $dirName . DIRECTORY_SEPARATOR . 'test';
+
+        $fileSystem->emptyDirectory($dirName);
+        
+        $this->assertEquals(true, is_dir($dirName));
+
+        file_put_contents($fileName, '');
+        $this->assertEquals(true, is_file($fileName));
+        $fileSystem->emptyDirectory($dirName);
+        $this->assertNotEquals(true, is_file($fileName));
+    }
+}
