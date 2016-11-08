61af170c2a322c05dedcefde7e2b9180b79b5711
Tatiana Poray <tanya@interprogma.ru>
2016-11-08 21:04:21 +0300

    ETPKIM-735 fix after review

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -12,6 +12,24 @@ namespace Cognitive\FileSystem;
 class FileSystem
 {
     const MAKE_DIR_MODE_DEFAULT = 0777;
+    const DIRECTORY_UP = '..';
+    const DIRECTORY_CURRENT = '.';
+    const PATH_PREFIX = '{^([0-9a-z]+:(?://(?:[a-z]:)?)?)}i';
+    /** @var string */
+    protected $dirSeparator = DIRECTORY_SEPARATOR;
+
+    /**
+     * Set directory separator.
+     *
+     * @param string $directorySeparator Directory separator.
+     *
+     * @return void
+     */
+    public function setDirectorySeparator($directorySeparator)
+    {
+        $this->dirSeparator = $directorySeparator;
+    }
+
     /**
      * Empty directory content.
      *
@@ -35,7 +53,7 @@ class FileSystem
                 } elseif ($file->isFile()) {
                     unlink($file->getRealPath());
                 } elseif ($file->isLink()) {
-                    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+                    if ($this->isOSWindows()) {
                         rmdir($file->getPathName());
                     } else {
                         unlink($file->getPathName());
@@ -76,11 +94,48 @@ class FileSystem
      */
     public function isAbsolutePath($path)
     {
-        return substr($path, 0, 1) === '/' || substr($path, 1, 1) === ':';
+        $path = $this->getPathWithoutPrefix($path);
+        return (0 === strpos($path, $this->dirSeparator));
+    }
+
+    /**
+     * Get path without prefix and replace directory separators. For example 'http://' or 'C:'.
+     *
+     * @param string $path Path.
+     *
+     * @return string
+     */
+    private function getPathWithoutPrefix($path)
+    {
+        $prefix = $this->getPathPrefix($path);
+        if ('' !== $prefix) {
+            $path = substr($path, strlen($prefix));
+        }
+        $path = strtr($path, '\\', $this->dirSeparator);
+        $path = strtr($path, '/', $this->dirSeparator);
+        return $path;
+    }
+
+    /**
+     * Get path prefix.
+     *
+     * @param string $path Path.
+     *
+     * @return string
+     */
+    private function getPathPrefix($path)
+    {
+        if (preg_match(self::PATH_PREFIX, $path, $match)) {
+            list(, $prefix) = $match;
+            return $prefix;
+        }
+        return '';
     }
 
     /**
      * Normalize path.
+     * This replaces backslashes and slashes with dirSeparator @see setDirectorySeparator,
+     * removes ending slash and collapses redundant separators and up-level references.
      *
      * @param string $path Path.
      *
@@ -90,32 +145,23 @@ class FileSystem
     public function normalizePath($path)
     {
         $parts = array();
-        $path = strtr($path, '\\', '/');
-        $prefix = '';
-        $absolute = false;
 
-        if (preg_match('{^([0-9a-z]+:(?://(?:[a-z]:)?)?)}i', $path, $match)) {
-            $prefix = $match[1];
-            $path = substr($path, strlen($prefix));
-        }
-
-        if (substr($path, 0, 1) === '/') {
-            $absolute = true;
-            $path = substr($path, 1);
-        }
+        $absolute = $this->isAbsolutePath($path);
+        $prefix = $this->getPathPrefix($path);
+        $path = $this->getPathWithoutPrefix($path);
 
         $up = false;
-        foreach (explode('/', $path) as $chunk) {
-            if ('..' === $chunk && ($absolute || $up)) {
+        foreach (explode($this->dirSeparator, $path) as $chunk) {
+            if (self::DIRECTORY_UP === $chunk && ($absolute || $up)) {
                 array_pop($parts);
-                $up = !(empty($parts) || '..' === end($parts));
-            } elseif ('.' !== $chunk && '' !== $chunk) {
+                $up = !(empty($parts) || self::DIRECTORY_UP === end($parts));
+            } elseif (self::DIRECTORY_CURRENT !== $chunk && '' !== $chunk) {
                 $parts[] = $chunk;
-                $up = '..' !== $chunk;
+                $up = (self::DIRECTORY_UP !== $chunk);
             }
         }
 
-        return $prefix.($absolute ? '/' : '').implode('/', $parts);
+        return $prefix.($absolute ? $this->dirSeparator : '').implode($this->dirSeparator, $parts);
     }
 
     /**
@@ -126,44 +172,45 @@ class FileSystem
      * @param bool   $directories Is directories.
      *
      * @return string
-     * @throws \FileSystemException If arguments wrong.
+     * @throws FileSystemException If arguments wrong.
      * @since 0.0.3 introduced.
      */
     public function findShortestPath($from, $to, $directories = false)
     {
         if (!$this->isAbsolutePath($from) || !$this->isAbsolutePath($to)) {
-            throw new \FileSystemException(sprintf('$from (%s) and $to (%s) must be absolute paths.', $from, $to));
+            throw new FileSystemException(sprintf('$from (%s) and $to (%s) must be absolute paths.', $from, $to));
         }
 
         $from = lcfirst($this->normalizePath($from));
         $to = lcfirst($this->normalizePath($to));
 
         if ($directories) {
-            $from = rtrim($from, '/') . '/dummy_file';
+            $from = rtrim($from, $this->dirSeparator) . $this->dirSeparator . 'dummy_file';
         }
 
         if (dirname($from) === dirname($to)) {
-            $shortestPath = './'.basename($to);
+            $shortestPath = self::DIRECTORY_CURRENT . $this->dirSeparator . basename($to);
         } else {
             $commonPath = $to;
-            while (strpos($from . '/', $commonPath . '/') !== 0 &&
-                '/' !== $commonPath &&
+            while (strpos($from . $this->dirSeparator, $commonPath . $this->dirSeparator) !== 0 &&
+                $this->dirSeparator !== $commonPath &&
                 !preg_match('{^[a-z]:/?$}i', $commonPath)
             ) {
-                $commonPath = strtr(dirname($commonPath), '\\', '/');
+                $commonPath = strtr(dirname($commonPath), '\\', $this->dirSeparator);
+                $commonPath = strtr($commonPath, '/', $this->dirSeparator);
             }
 
-            if (0 !== strpos($from, $commonPath) || '/' === $commonPath) {
+            if (0 !== strpos($from, $commonPath) || $this->dirSeparator === $commonPath) {
                 $shortestPath = $to;
             } else {
-                $commonPath = rtrim($commonPath, '/') . '/';
-                $sourcePathDepth = substr_count(substr($from, strlen($commonPath)), '/');
-                $commonPathCode = str_repeat('../', $sourcePathDepth);
+                $commonPath = rtrim($commonPath, $this->dirSeparator) . $this->dirSeparator;
+                $sourcePathDepth = substr_count(substr($from, strlen($commonPath)), $this->dirSeparator);
+                $commonPathCode = str_repeat(self::DIRECTORY_UP . $this->dirSeparator, $sourcePathDepth);
 
-                $shortestPath = ($commonPathCode . substr($to, strlen($commonPath))) ?: './';
+                $shortestPath = ($commonPathCode . substr($to, strlen($commonPath))) ?
+                    : self::DIRECTORY_CURRENT . $this->dirSeparator;
             }
         }
-        $shortestPath = strtr($shortestPath, '/', DIRECTORY_SEPARATOR);
         return $shortestPath;
     }
 
@@ -182,7 +229,7 @@ class FileSystem
 
         $relativePath = $this->findShortestPath($target, $link);
         chdir(dirname($target));
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        if ($this->isOSWindows()) {
             $command = 'mklink /d';
             exec("$command $relativePath $target", $output, $returnVar);
             $result = $returnVar > 0 ? false : true;
@@ -191,5 +238,15 @@ class FileSystem
         }
         chdir($cwd);
         return (bool)$result;
+    }
+
+    /**
+     * Is Windows?
+     *
+     * @return bool
+     */
+    private function isOSWindows()
+    {
+        return strtoupper(substr(PHP_OS, 0, strlen('WIN'))) === 'WIN';
     }
 }
